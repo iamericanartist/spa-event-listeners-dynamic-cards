f785f47c4ce8127052d245540d905b5a46d227f7
Eric Denton <eric@iamericanartist.com>
2016-06-13 11:26:15 -0500

    saving progress

--- cards.js
+++ cards.js
@@ -1,1 +1,16 @@
 console.log("cards.js is loaded");
+
+// 1 Create an HTML page that contains a text area and a button labeled Create.
+	// nothing needed	
+var cards = [];
+var textInputEl = document.getElementById("textInput");
+console.log("textInputEl", textInputEl);
+
+
+
+// 2 When the user enters in text into the text area and then clicks the create button, 
+// create a new card element in the DOM. You decide the height/width of the card.
+
+
+// 3 When the user clicks the Delete button, the containing card, and no other cards, 
+// should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
