c2992989d9c8f77c3ca3182725613c67a7d8e801
Eric Denton <eric@iamericanartist.com>
2016-06-17 00:49:59 -0500

    got the counting id's working

--- cards.js
+++ cards.js
@@ -22,6 +22,46 @@ createButton.addEventListener("click", function createButtonClick (event){
   makeCard();
   console.log("textInputEl", textInputEl.value);
 });
+
+function makeCard() {
+        // // initialize all variables scoped to function
+    var divEl = document.createElement("div"),
+        lineEl = document.createElement("hr"),
+        delButtonEl = document.createElement("button"),
+        buttonText = document.createTextNode("Delete This Card!"),
+        userInput = document.createTextNode(textInputEl.value); //end variable adding (, and ; help DRY up code)
+
+        counter++; // Our counter within the function, for assigning unique IDs to each card/delete button
+        divEl.id = `domCard-${counter}`; // Setting "domCard-#" ids for each new CARD
+        divEl.appendChild(userInput); //adding the userInput value to the CARD
+        divEl.appendChild(lineEl); //adding the "hr" element to the CARD
+        divEl.appendChild(delButtonEl); //adding the delete button to the CARD
+        divEl.classList.add("domCard"); //gives the CARDS a class of "domCard" for styling
+        // console.log("Log", divEl.classList)
+        // console.log("Log", divEl.classList.value)
+        delButtonEl.appendChild(buttonText); //gives the DELETE BUTTON it's text from above
+        delButtonEl.id = `delDomCard-${counter}`; //gives each DELETE BUTTON a numbered ID
+        delButtonEl.classList.add("domCardDeleteButton"); //gives DELETE BUTTONS a class of "domCardDeleteButton" for styling
+        // console.log("delButtonEl Values", delButtonEl.classList)
+        // console.log("delButtonEl Values", delButtonEl.classList.value)
+        outputAreaEl.appendChild(divEl);
+}
+
+// 3 When the user clicks the Delete button, the containing card, and no other cards, 
+  // should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
+
+
+
+
+
+textInputEl.addEventListener("keyup", ifEnterKey);
+
+
+
+
+
+
+// // Part 2:
 // // String Interpolation method:
 // function makeCard() {
 //   outputAreaEl.innerHTML += ` You clicked on the ${event.target.innerHTML} section</br>`;
@@ -29,30 +69,3 @@ createButton.addEventListener("click", function createButtonClick (event){
 //   outputAreaEl.innerHTML += `<div id="domCard[i]" class="domCard">${textInputEl.value}<hr/>
 //                               <button id="deleteButton[i]" class="buttons">Delete </button></div>`;
 // }
-
-function makeCard() {
-    var divEl = document.createElement("div"),
-        lineEl = document.createElement("hr"),
-        delButtonEl = document.createElement("button"),
-        buttonText = document.createTextNode("Delete This Card!"),
-        userInput = document.createTextNode(textInputEl.value);
-        // counter = 0;
-
-        counter++;
-        divEl.id = `domCard-${counter}`;
-        divEl.appendChild(userInput);
-        divEl.appendChild(lineEl);
-        divEl.appendChild(delButtonEl);
-        divEl.classList.add("domCard");
-
-        delButtonEl.appendChild(buttonText);
-        delButtonEl.id = `delDomCard-${counter}`;
-
-        outputAreaEl.appendChild(divEl);
-}
-
-
-// 3 When the user clicks the Delete button, the containing card, and no other cards, 
-  // should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
-
-textInputEl.addEventListener("keyup", ifEnterKey);
